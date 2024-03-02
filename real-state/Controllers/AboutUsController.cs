bb57483707953ab2921dd4740d191848c8b234eb
Sheetashma <sheetashma2072@gmail.com>
2024-03-02 16:47:29 +0545

    Initial push

---
+++ real-state/Controllers/AboutUsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace real_state.Controllers
+{
+    public class AboutUsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
